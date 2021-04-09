027881ea74193ece475b40773e6cb0aaba3d8621
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-04-09 14:25:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //importing the files
 const meme = require("./lib/Meme");
 //function for generating a new meme
-Meme = (token) => { return new meme(token) }
-
+function Meme(token) {
+    return new meme(token);
+}
 Meme.Memer = meme;
 Meme.version = require("./package.json").version;
 
