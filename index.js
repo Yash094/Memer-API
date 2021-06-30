8eba550f450263166bdf8700a42363f8783264fb
Yash <67926590+Yash094@users.noreply.github.com>
2021-06-30 13:04:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 //importing the files
 const meme = require("./lib/Meme");
-const Util = require("./lib/Util");
-/**
-  * @ {*} param token | Memer-api Token {string}
-  * returns {Memer-Api Class}
- */
+//function for generating a new meme
 function Meme(token) {
     return new meme(token);
 }
-//add Options to it
 Meme.Memer = meme;
-Meme.Util = Util;
 Meme.version = require("./package.json").version;
-Meme.author = "YASH & Tomato#6966",
 
 //exporting this meme
 module.exports = Meme;
-module.exports.Util = Util;
