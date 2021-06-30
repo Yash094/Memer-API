d3594ee10b2b5629dae402ac7755fc5f90b072a4
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-06-30 09:37:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //importing the files
 const meme = require("./lib/Meme");
-//function for generating a new meme
+/**
+  * @param {string} token | Memer-api Token {string}
+  * returns {Memer-Api Class}
+ */
 function Meme(token) {
     return new meme(token);
 }
+//add Options to it
 Meme.Memer = meme;
 Meme.version = require("./package.json").version;
-
+Meme.author = "YASH & Tomato#6966",
 //exporting this meme
 module.exports = Meme;
